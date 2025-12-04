9a7267eb09f61ca4ecad783eaffe4a98e4a4117a
AamodJ <aamodj23@gmail.com>
2025-12-04 20:13:02 +0400

    Completed day 4

---
+++ day4/day4p2.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+with open("input.txt", "r") as fd:
+    s = fd.readlines()
+
+
+def count_neighbours(locs: np.ndarray, x: int, y: int) -> int:
+    neighbour_count = 0
+    for offsetY in [-1, 0, 1]:
+        for offsetX in [-1, 0, 1]:
+            if (
+                y + offsetY not in range(locs.shape[0])
+                or x + offsetX not in range(locs.shape[1])
+                or (offsetX == 0 and offsetY == 0)
+            ):
+                continue
+            elif locs[x + offsetX, y + offsetY] == "@":
+                neighbour_count += 1
+                if x == 2 and y == 0:
+                    print(
+                        f"Found neighbour at {x + offsetX}, {y + offsetY} for {x}, {y}"
+                    )
+
+    return neighbour_count
+
+
+locs = np.array([[col for col in row] for row in s])[:, :-1]
+next_locs = locs.copy()
+total_accessible_rolls = 0
+
+while True:
+    for y in range(locs.shape[0]):
+        for x in range(locs.shape[1]):
+            if locs[x, y] == "@" and count_neighbours(locs, x, y) < 4:
+                total_accessible_rolls += 1
+                next_locs[x, y] = "."
+    if (locs == next_locs).all():
+        break
+    else:
+        locs = next_locs.copy()
+
+
+print(f"Number of accessible paper rolls: {total_accessible_rolls}")
